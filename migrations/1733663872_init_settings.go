6e285d798e6b9330d6ed599eb5ad74260ea2aed6
Blazej Sewera <code@sewera.dev>
2024-12-10 21:34:17 +0100

    fix: remove app url from go migration code

--- migrations/1733663872_init_settings.go
+++ migrations/1733663872_init_settings.go
@@ -3,7 +3,6 @@ package migrations
 import (
 	"github.com/pocketbase/pocketbase/core"
 	m "github.com/pocketbase/pocketbase/migrations"
-	"github.com/sewera/gift-goose/config"
 	"github.com/sewera/gift-goose/log"
 )
 
@@ -11,7 +10,6 @@ func init() {
 	m.Register(func(app core.App) error {
 		settings := app.Settings()
 		settings.Meta.AppName = "Gift Goose"
-		settings.Meta.AppURL = config.GetHostFromJSONOrReturnDefault().String()
 		log.Info("settings initialized")
 		return nil
 	}, func(app core.App) error {
